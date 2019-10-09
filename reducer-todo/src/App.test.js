ca9584ea8333c563fb61feb05fbefbb70976a3ec
Roy Wakumelo Jr <roywakumelo@roys-mbp.attlocal.net>
2019-10-09 02:38:03 -0400

    Added A Snapshot SummaryTest

--- reducer-todo/src/App.test.js
+++ reducer-todo/src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { render } from '@testing-library/react';
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it ('renders', () => {
+  const {asFragment} = render (<App text = 'Hello from the world of todos!' />);
+  expect(asFragment()).toMatchSnapshot();
+});
+
