65a638bd10730269b6af020dc5cb244a12907a23
Roy Wakumelo Jr <roywakumelo@roys-mbp.attlocal.net>
2019-10-16 16:51:52 -0400

    Updated File Layout

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1> Reducer Todo App </h1>
+        <h1> Reducer Todo App! </h1>
         <TodoList />
       </header>
     </div>
